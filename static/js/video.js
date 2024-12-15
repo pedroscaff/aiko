29757490e7d93ad8a118d7f28ce3799e7cd84d19
Pedro Scaff <pedro@sanctuary.computer>
2024-12-15 14:51:18 +0100

    Video section

---
+++ static/js/video.js
@@ -0,0 +1,9 @@
+function handleVideoPlay(target, videoId) {
+  console.log(target, videoId);
+  const container = target.getElementsByClassName('video-iframe')[0];
+  const iframe = container.getElementsByTagName('iframe')[0];
+  iframe.setAttribute('src', `${iframe.src}?autoplay=1`);
+  container.classList.remove('video-disabled');
+  const imgPreview = target.getElementsByClassName('video-preview-container')[0];
+  imgPreview.classList.add('video-disabled');
+}
