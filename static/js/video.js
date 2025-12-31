3090f1bef2673ecd862cc7d9443dd043daa27127
Pedro Scaff <pedro@sanctuary.computer>
2025-12-31 13:01:05 +0100

    Direct video to new tab to avoid GDPR cookie

--- static/js/video.js
+++
@@ -1,8 +0,0 @@
-function handleVideoPlay(target) {
-  const container = target.getElementsByClassName('video-iframe')[0];
-  const iframe = container.getElementsByTagName('iframe')[0];
-  iframe.setAttribute('src', `${iframe.src}?autoplay=1`);
-  container.classList.remove('video-disabled');
-  const imgPreview = target.getElementsByClassName('video-preview-container')[0];
-  imgPreview.classList.add('video-disabled');
-}
